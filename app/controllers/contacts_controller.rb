8108adb86e2bf91a4b9adc3f44f748ae6f96d698
Sean Lynch <sean.lynch@thrivecommerce.com>
2018-09-17 12:33:51 -0400

    added model validation and flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,18 @@
 class ContactsController < ApplicationController
-	before_action :load_user, only: [:create]
+  before_action :load_user, only: [:create]
 
-	def create
-		@contact = @user.contacts.create(contact_params)
-		redirect_to user_path(@user)
-	end
-
-	private
-	def load_user
-    @user = User.find(params[:id])
+  def create
+    @contact = @user.contacts.create(contact_params)
+    flash[:notice] = "Successfully created contact"
+    redirect_to user_path(@user)
   end
 
-	def contact_params
-		params.require(:contact).permit(:name, :email)
-	end
+  private
+  def load_user
+    @user = User.find(params[:user_id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 	has_many :contacts
+  validates :email, :password, presence: true, on: :create
 end
