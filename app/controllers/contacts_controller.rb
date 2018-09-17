5b9fb91c49350b506a95d83bc646b9b5255b045a
Sean Lynch <sean.lynch@thrivecommerce.com>
2018-09-17 11:39:46 -0400

    changed tabs to spaces

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,17 @@
 class ContactsController < ApplicationController
-	before_action :load_user, only: [:create]
+  before_action :load_user, only: [:create]
 
-	def create
-		@contact = @user.contacts.create(contact_params)
-		redirect_to user_path(@user)
-	end
+  def create
+    @contact = @user.contacts.create(contact_params)
+    redirect_to user_path(@user)
+  end
 
-	private
-	def load_user
+  private
+  def load_user
     @user = User.find(params[:id])
   end
 
-	def contact_params
-		params.require(:contact).permit(:name, :email)
-	end
+  def contact_params
+    params.require(:contact).permit(:name, :email)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	# before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :load_user, except: [:index, :new, :create]
 
   def index
@@ -7,48 +7,48 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-	def show
-		# @user = User.find(params[:id])
+  def show
+    # @user = User.find(params[:id])
     # render json: @user, root: true
-	end
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-		@user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-		@user.save
-		redirect_to @user
-	end
+    @user.save
+    redirect_to @user
+  end
 
-	def update
-		uploaded_io = params[:user][:contacts_file]
+  def update
+    uploaded_io = params[:user][:contacts_file]
 
     filepath = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
 
-		File.open(filepath, 'wb') do |file|
-			file.write(uploaded_io.read)
+    File.open(filepath, 'wb') do |file|
+      file.write(uploaded_io.read)
     end
 
     File.foreach(filepath, "BEGIN:VCARD") do |card|
       vcard = VCardigan.parse(card)
 
-	    unless vcard.fn.nil? || vcard.tel.nil?
-	      contact_params = { name: vcard.fn[0].values[0], email: vcard.tel[0].values[0] }
-	      @user.contacts.create(contact_params)
-	    end
+      unless vcard.fn.nil? || vcard.tel.nil?
+        contact_params = { name: vcard.fn[0].values[0], email: vcard.tel[0].values[0] }
+        @user.contacts.create(contact_params)
+      end
     end
 
     redirect_to @user
-	end
+  end
 
-	private
+  private
   def load_user
     @user = User.find(params[:id])
   end
 
-	def user_params
-		params.require(:user).permit(:username, :password)
-	end
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
